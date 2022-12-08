f27ca4ba1b4e514fd59296ff47e30bcf57a045ba
Cephalopod <elitephycho@yahoo.com>
2022-12-08 17:01:02 -0500

    Update Scraper

--- src/scraper.py
+++ src/scraper.py
@@ -3,15 +3,6 @@ import lxml.html
 import re
 from bs4 import BeautifulSoup
 import urllib3
-import json
-
-'''
-Breakdown:
-    - Load site data
-    - Parse site data based on needs criteria
-    - Determine if site data loaded is valid enough based on acceptance criteria
-    - If data doesn't meet acceptance criteria try again until 'tries' are exhausted
-'''
 
 class WebScraper:
     def __init__(self, **kwargs) -> None:
@@ -19,6 +10,7 @@ class WebScraper:
         self.loadedData = []
         self.parser = None if 'parser' not in kwargs else kwargs['parser']
         self.accepter = None if 'accepter' not in kwargs else kwargs['accepter']
+        self.interpreter = None if 'interpreter' not in kwargs else kwargs['interpreter']
         self.collectedData = {}
 
     def scrapePage(self, url=None, tries=1):
@@ -35,22 +27,11 @@ class WebScraper:
                 div = BeautifulSoup(html.raw.data).get_text()
                 return None, None
             self.loadedData = re.split(r'<|>', str(lxml.html.tostring(div[0])))
-            self.collectedData = self.parser(self.collectedData, self.loadedData)
-            if self.accepter(self.collectedData): return self.collectedData
-            tries -= 1
+            if self.parser is not None: self.collectedData = self.parser(self.collectedData, self.loadedData)
+            if self.accepter is not None:
+                if self.accepter(self.collectedData): break
+                tries -= 1
+            else: break
+        if self.interpreter is not None: 
+            return self.interpreter(self.collectedData)
         return self.collectedData
-
-def loadDataFromJson(loadFile):
-    fileJson = {}
-    with open(loadFile) as json_file:
-        fileJson = json.load(json_file)
-    return fileJson
-
-def accepter(data) -> bool:
-    pass
-
-def parser(data, stringList) -> dict:
-    pass
-
-def interpreter():
-    pass
